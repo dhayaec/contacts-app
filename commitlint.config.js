d42976e8897af4b19a647f63fad8ef4cd3a87b04
Dhayanandhan <dhayaec@gmail.com>
2026-08-28 17:00:57 +0530

    fix(oidc): add GitHubActionsOIDC role, OIDC provider, inline policy

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'ci', 'build', 'revert']],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'header-max-length': [2, 'always', 100],
+  },
+};
